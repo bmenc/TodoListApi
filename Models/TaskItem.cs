d8c2c52e6133f96fb0ccdddda7f27a8b1895286b
bmenc <bmenchaca.cca@gmail.com>
2025-02-18 17:43:38 -0600

    Agregar archivos de proyecto.

---
+++ Models/TaskItem.cs
@@ -0,0 +1,9 @@
+﻿namespace TodoListApi.Models
+{
+    public class TaskItem
+    {
+        public int Id { get; set; }
+        public required string Title { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
